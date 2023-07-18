ceec8eb0c14675ffa42b2ffd61ae6d6d96856894
YASWANTH BATTU <83590022+s-1-n-t-h@users.noreply.github.com>
2023-07-18 15:41:45 +0530

    delete request and code smells fix

--- using express library/index.js
+++ using express library/index.js
@@ -18,18 +18,13 @@ app.get("/api/courses", (req, res) => {
 });
 app.get("/api/courses/:id", (req, res) => {
 	const course = courses.find((c) => c.id === parseInt(req.params.id));
-	if (!course) {
-		res.status(404).send("The course with the given ID is not found");
-		return;
-	}
+	if (!course)
+		return res.status(404).send("The course with the given ID is not found");
 	res.send(course);
 });
 app.post("/api/courses", (req, res) => {
 	const { error } = validateCourse(req.body);
-	if (error) {
-		res.status(400).send(error.details[0].message);
-		return;
-	}
+	if (error) return res.status(400).send(error.details[0].message);
 	const course = {
 		id: courses.length + 1,
 		name: req.body.name,
@@ -39,19 +34,21 @@ app.post("/api/courses", (req, res) => {
 });
 app.put("/api/courses/:id", (req, res) => {
 	const course = courses.find((c) => c.id === parseInt(req.params.id));
-	if (!course) {
-		res.status(404).send("The course with the given ID is not found");
-		return;
-	}
+	if (!course)
+		return res.status(404).send("The course with the given ID is not found");
 	const { error } = validateCourse(req.body);
-	if (error) {
-		res.status(400).send(error.details[0].message);
-		return;
-	}
+	if (error) return res.status(400).send(error.details[0].message);
 	course.name = req.body.name;
 	res.send(course);
 });
-
+app.delete("/api/courses/:id", (req, res) => {
+	const course = courses.find((c) => c.id === parseInt(req.params.id));
+	if (!course)
+		return res.status(404).send("The course with the given ID is not found");
+	const index = courses.indexOf(req.params.id);
+	courses.splice(index, 1);
+	return res.send(course);
+});
 function validateCourse(course) {
 	const schema = {
 		name: Joi.string().min(3).required(),
