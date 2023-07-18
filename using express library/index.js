71bf690e13a31a0894359fd55f813dc1ff800655
YASWANTH BATTU <83590022+s-1-n-t-h@users.noreply.github.com>
2023-07-18 14:27:07 +0530

    put request fro updation of courses and object destructring, few code smells

--- using express library/index.js
+++ using express library/index.js
@@ -5,9 +5,6 @@ const Joi = require('joi')
 
 app.use(express.json())
 
-const schema = {
-    name: Joi.string().min(3).required()
-}
 const courses = [
     {id: 1, name:'Practical Data Science'},
     {id: 2, name:'AI for Everybody'},
@@ -28,18 +25,38 @@ app.get('/api/courses/:id',(req, res) => {
     res.send(course)
 })
 app.post('/api/courses', (req, res) => {
-    const result = Joi.validate(req.body, schema)
-    
-    if( result.error ){ 
-        res.status(400).send(result.error.details[0].message) 
-        return
+    const { error } = validateCourse(req.body);
+    if (error) {
+        res.status(400).send(error.details[0].message);
+        return;
     }
     const course = {
-        id: courses.length+1,
-        name: req.body.name
+        id: courses.length + 1,
+        name: req.body.name,
+    };
+    courses.push(course);
+    res.send(course);
+})
+app.put('/api/courses/:id', (req, res) => {
+    const course = courses.find((c) => c.id === parseInt(req.params.id));
+    if (!course) {
+        res.status(404).send("The course with the given ID is not found");
+        return;
     }
-    courses.push(course)
+    const {error} = validateCourse(req.body)
+    if (error) {
+        res.status(400).send(error.details[0].message);
+        return;
+    }
+    course.name = req.body.name
     res.send(course)
 })
+
+function validateCourse(course){
+    const schema = {
+        name: Joi.string().min(3).required(),
+    };
+    return Joi.validate(course,schema)
+}
 const port = process.env.PORT || 3000
 app.listen(port, () => console.log(`Listening on port ${port}`))
