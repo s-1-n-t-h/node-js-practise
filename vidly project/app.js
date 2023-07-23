231c71c8fbce6adbe9833d6e8e20b32c6432c8e5
YASWANTH BATTU <83590022+s-1-n-t-h@users.noreply.github.com>
2023-07-23 19:29:37 +0530

    using express router & refactoring app for vidley

--- vidly project/app.js
+++ vidly project/app.js
@@ -1,12 +1,64 @@
 const express = require("express");
 app = express();
 const Joi = require("joi");
-const geners = require("./routes/geners");
-const home = require("./routes/home");
-
 app.use(express.json());
-app.use("/", home);
-app.use("/api/geners", geners);
+const geners = [
+	{ id: 1, name: "Horror" },
+	{ id: 2, name: "Romance" },
+	{ id: 3, name: "Thriller" },
+	{ id: 4, name: "Action" },
+];
+
+app.get("/", (req, res) => {
+	res.send("<h1>Welcome to Vidly!</h1>");
+});
+
+app.get("/api/geners", (req, res) => {
+	res.send(geners);
+});
+
+app.get("/api/geners/:id", (req, res) => {
+	const gener = geners.find((g) => g.id === parseInt(req.params.id));
+	if (!gener) {
+		return res.status(404).send("The gener with given ID is not found");
+	}
+	res.send(gener);
+});
+
+app.post("/api/geners", (req, res) => {
+	const { error } = validateGener(req.body);
+	if (error) return res.status(400).send(error.details[0].message);
+	const gener = {
+		id: geners.length + 1,
+		name: req.body.name,
+	};
+	geners.push(gener);
+	return res.send(gener);
+});
+
+app.put("/api/geners/:id", (req, res) => {
+	const gener = geners.find((g) => g.id === parseInt(req.params.id));
+	if (!gener)
+		return res.status(404).send("The resounce with the given ID is not found");
+	const { error } = validateGener(req.body);
+	if (error) return res.status(400).send(error.deatils[0].message);
+	gener.name = req.body.name;
+	res.send(gener);
+});
+app.delete("/api/geners/:id", (req, res) => {
+	const gener = geners.find((g) => g.id === parseInt(req.params.id));
+	if (!gener)
+		return res.status(404).send("The resource with the given ID is not found");
+	const index = geners.indexOf(gener);
+	geners.splice(index, 1);
+	return res.send(gener);
+});
+function validateGener(gener) {
+	const schema = {
+		name: Joi.string().min(3).required(),
+	};
+	return Joi.validate(gener, schema);
+}
 
 const port = process.env.port || 3000;
 app.listen(port, () => console.log(`listening on ${port}`));
